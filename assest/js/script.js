712b2a8a8308b5a8b7f819bfb3a0bee96ce19d64
Giuseppe99a <115545945+Giuseppe99a@users.noreply.github.com>
2023-05-24 10:28:13 +0200

    ventunesimo commit

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,17 +1,13 @@
-function addElement() {
-  // create a new div element
-  const newDiv = document.createElement("div");
 
-  // and give it some content
-  const newContent = document.createTextNode("0");
+var container = document.createElement("div")
+container.className = "container";
+var number =document.createElement("input")
+number.id="value";
+number.textContent="0";
+container.appendChild(number);
+document.body.appendChild(container);
 
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);
 
-  // add the newly created element and its content into the DOM
-  const currentDiv = document.getElementById("div");
-  document.body.insertBefore(newDiv, currentDiv);
-}
 var container = document.createElement("div");
 container.className = "container";
 
