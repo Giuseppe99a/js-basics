df936f59bfbd8470b96a81a0dcbd2997bdfa5240
Giuseppe99a <115545945+Giuseppe99a@users.noreply.github.com>
2023-05-24 10:35:44 +0200

    ventiquattresimo commit

--- assest/js/script.js
+++ assest/js/script.js
@@ -10,68 +10,39 @@ document.body.appendChild(container);
 
 var container = document.createElement("div");
 container.className = "container";
-
-// Creazione del pulsante con ID "reset"
 var plusBtn = document.createElement("button");
 plusBtn.id = "plus";
 plusBtn.textContent = "+";
-
-// Aggiunta del pulsante all'elemento <div>
 container.appendChild(plusBtn);
-
-// Aggiunta dell'elemento <div> al documento
 document.body.appendChild(container);
-
 plusBtn.addEventListener('click', plusEvent); 
-
-
-
 function plusEvent() {
-  
   const elem = document.getElementById("Counter");
   value++;
   number.innerHTML = value;
 }
-// Creazione del pulsante con ID "reset"
+
 var minusBtn = document.createElement("button");
-minusBtn.id = "Minus";
+minusBtn.id = "minus";
 minusBtn.textContent = "-";
-
-// Aggiunta del pulsante all'elemento <div>
 container.appendChild(minusBtn);
-
-// Aggiunta dell'elemento <div> al documento
 document.body.appendChild(container);
-
 minusBtn.addEventListener('click', minusEvent);
-
-
-
 function minusEvent() {
-  
   const elem = document.getElementById("Counter");
   value--;
   number.innerHTML = value;
 }  
 
-// Creazione del elemento <div> con classe "container"
+
 var container = document.createElement("div");
 container.className = "container";
-
-// Creazione del pulsante con ID "reset"
 var resetBtn = document.createElement("button");
 resetBtn.id = "reset";
 resetBtn.textContent = "AZZERRA";
-
-// Aggiunta del pulsante all'elemento <div>
 container.appendChild(resetBtn);
-
-// Aggiunta dell'elemento <div> al documento
 document.body.appendChild(container);
-
 resetBtn.addEventListener('click', resetEvent);
-
-
 function resetEvent() {
   const elem = document.getElementById("Counter");
   value = 0;
