f46dd76cb0791b32ab91fe8b7c18240f7d4251a9
Giuseppe99a <115545945+Giuseppe99a@users.noreply.github.com>
2023-05-24 10:08:51 +0200

    sedicesimo commit

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,3 +1,19 @@
+document.body.onload = addElement;
+
+function addElement() {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("0");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("div");
+  document.body.insertBefore(newDiv, currentDiv);
+}
 
 var container = document.createElement("div");
 container.className = "container";
