62ca9a588c747709c2a1aa0f717128e5e42cc919
Giuseppe99a <115545945+Giuseppe99a@users.noreply.github.com>
2023-05-24 10:12:11 +0200

    diciottesimo commit

--- assest/js/script.js
+++ assest/js/script.js
@@ -2,17 +2,17 @@ document.body.onload = addElement;
 
 function addElement() {
   // create a new div element
-  const newDiv = document.createElement("div");
+  const div = document.createElement("div");
 
   // and give it some content
-  const newContent = document.createTextNode("0");
+  const value = document.createTextNode("0");
 
   // add the text node to the newly created div
-  newDiv.appendChild(newContent);
+  div.appendChild(value);
 
   // add the newly created element and its content into the DOM
   const currentDiv = document.getElementById("div");
-  document.body.insertBefore(newDiv, currentDiv);
+  document.body.insertBefore(div, value);
 }
 
 var container = document.createElement("div");
